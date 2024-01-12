117a10eb58f1c90ecad4c9e4803125f4921b6874
Kashan Shaikh <shaikhkashan670@gmail.com>
2024-01-12 14:14:02 -0800

    stevta login change

--- resources/views/main_super_admin/dashboard/institutes/Insitute_login.blade.php
+++ resources/views/main_super_admin/dashboard/institutes/Insitute_login.blade.php
@@ -1,144 +1,188 @@
 <!DOCTYPE html>
-<html lang="en" dir="ltr">
+<html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title> Institute Login </title>
+    <title>Stevta Login</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
     <style>
-        @import url('https://fonts.googleapis.com/css?family=Poppins:400,500,600,700&display=swap');
+        @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap');
 
         * {
             margin: 0;
             padding: 0;
             box-sizing: border-box;
-            font-family: 'Poppins', sans-serif;
+            font-family: "Poppins", sans-serif;
         }
 
         body {
-            min-height: 100vh;
+            width: 100%;
+            height: 100vh;
             display: flex;
             align-items: center;
-            justify-content: left;
-            padding: 0 100px;
-            /* background: #4070f4; */
-            background: url('/institute_login/images/bg.jpg');
-            background-repeat: no-repeat;
-            background-size: cover;
-            background-position: center;
+            justify-content: center;
+            background: #059847;
+        }
+
+        ::selection {
+            color: #fff;
+            background: #059847;
         }
 
         .wrapper {
-
-
-            position: relative;
-            max-width: 430px;
-            width: 100%;
+            width: 380px;
+            padding: 18px 25px 15px 25px;
             background: #fff;
-            padding: 60px 34px;
-            border-radius: 6px;
-            box-shadow: 0 5px 10px rgba(0, 0, 0, 0.2);
+            border-radius: 5px;
+            text-align: center;
+            box-shadow: 10px 10px 15px rgba(0, 0, 0, 0.1);
         }
 
-        .wrapper h2 {
-            position: relative;
-            font-size: 22px;
+        .wrapper header {
+            font-size: 35px;
             font-weight: 600;
-            color: #333;
-        }
-
-        .wrapper h2::before {
-            content: '';
-            position: absolute;
-            left: 0;
-            bottom: 0;
-            height: 3px;
-            width: 28px;
-            border-radius: 12px;
-            background: #3b7052;
         }
 
         .wrapper form {
-            margin-top: 30px;
+            margin: 40px 0;
         }
 
-        .wrapper form .input-box {
-            height: 52px;
-            margin: 18px 0;
+        form .field {
+            width: 100%;
+            margin-bottom: 20px;
         }
 
-        form .input-box select {
+        form .field.shake {
+            animation: shake 0.3s ease-in-out;
+        }
+
+        @keyframes shake {
+
+            0%,
+            100% {
+                margin-left: 0px;
+            }
+
+            20%,
+            80% {
+                margin-left: -12px;
+            }
+
+            40%,
+            60% {
+                margin-left: 12px;
+            }
+        }
+
+        form .field .input-area {
+            height: 50px;
+            width: 100%;
+            position: relative;
+        }
+
+        form input {
+            width: 100%;
             height: 100%;
-            width: 100%;
             outline: none;
-            padding: 0 15px;
+            padding: 0 45px;
+            font-size: 18px;
+            background: none;
+            caret-color: #059847;
+            border-radius: 5px;
+            border: 1px solid #bfbfbf;
+            border-bottom-width: 2px;
+            transition: all 0.2s ease;
+        }
+
+        form .field input:focus,
+        form .field.valid input {
+            border-color: #059847;
+        }
+
+        form .field.shake input,
+        form .field.error input {
+            border-color: #dc3545;
+        }
+
+        .field .input-area i {
+            position: absolute;
+            top: 50%;
+            font-size: 18px;
+            pointer-events: none;
+            transform: translateY(-50%);
+        }
+
+        .input-area .icon {
+            left: 15px;
+            color: #bfbfbf;
+            transition: color 0.2s ease;
+        }
+
+        .input-area .error-icon {
+            right: 15px;
+            color: #dc3545;
+        }
+
+        form input:focus~.icon,
+        form .field.valid .icon {
+            color: #059847;
+        }
+
+        form .field.shake input:focus~.icon,
+        form .field.error input:focus~.icon {
+            color: #bfbfbf;
+        }
+
+        form input::placeholder {
+            color: #bfbfbf;
             font-size: 17px;
-            font-weight: 400;
-            color: #333;
-            border: 1.5px solid #C7BEBE;
-            border-bottom-width: 2.5px;
-            border-radius: 6px;
-            transition: all 0.3s ease;
-            background: #fff;
         }
 
-        form .input-box input {
-            height: 100%;
-            width: 100%;
-            outline: none;
-            padding: 0 15px;
-            font-size: 17px;
-            font-weight: 400;
-            color: #333;
-            border: 1.5px solid #C7BEBE;
-            border-bottom-width: 2.5px;
-            border-radius: 6px;
-            transition: all 0.3s ease;
+        form .field .error-txt {
+            color: #dc3545;
+            text-align: left;
+            margin-top: 5px;
         }
 
-        .input-box input:focus,
-        .input-box input:valid {
-            border-color: #3b7052;
+        form .field .error {
+            display: none;
         }
 
-        form .policy {
-            display: flex;
-            align-items: center;
+        form .field.shake .error,
+        form .field.error .error {
+            display: block;
         }
 
-        form h3 {
-            color: #707070;
-            font-size: 14px;
-            font-weight: 500;
-            margin-left: 10px;
+        form .pass-txt {
+            text-align: left;
+            margin-top: -10px;
         }
 
-        .input-box.button input {
-            color: #fff;
-            letter-spacing: 1px;
-            border: none;
-            background: #3b7052;
-            cursor: pointer;
-        }
-
-        .input-box.button input:hover {
-            background: #3b7052;
-        }
-
-        form .text h3 {
-            color: #333;
-            width: 100%;
-            text-align: center;
-        }
-
-        form .text h3 a {
-            color: #4070f4;
+        .wrapper a {
+            color: #059847;
             text-decoration: none;
         }
 
-        form .text h3 a:hover {
+        .wrapper a:hover {
             text-decoration: underline;
         }
 
+        form input[type="submit"] {
+            height: 50px;
+            margin-top: 30px;
+            color: #fff;
+            padding: 0;
+            border: none;
+            background: #059847;
+            cursor: pointer;
+            border-bottom: 2px solid rgba(0, 0, 0, 0.1);
+            transition: all 0.3s ease;
+        }
+
+        form input[type="submit"]:hover {
+            background: #047135;
+        }
         .alert {
             position: relative;
             padding: 0.75rem 1.25rem;
@@ -152,32 +196,41 @@
             background-color: #f8d7da;
             border-color: #f5c6cb;
         }
-    </style> 
+    </style>
+</head>
+
+<body>
     <div class="wrapper">
-        <h2>Institute Login</h2>
+        <img src="/new_template/images/stevta-logo-removebg.png" alt="" width="100px">
+        <header>Stevta Login</header>
         <form action="{{ route('institute-logged') }}" method="POST">
             @csrf
-			@if(@session('ints-message'))
-                    <div class="alert alert-danger">
-                        <strong>Opps!</strong> {{ @session('ints-message') }}
-                    </div>
-                @endif
-            <div class="input-box">
-                <input type="text" id="username" name="Institute_username" placeholder="Enter username" required>
+            @if(@session('ints-message'))
+                <div class="alert alert-danger">
+                    <strong>Opps!</strong> {{ @session('ints-message') }}
+                </div>
+            @endif
+            <div class="field email">
+                <div class="input-area">
+                    <input type="text" id="username"  name="Institute_username" placeholder="Enter Username">
+                    <i class="icon fas fa-user"></i>
+                    <i class="error error-icon fas fa-exclamation-circle"></i>
+                </div>
+                <div class="error error-txt">Username can't be blank</div>
             </div>
-            <div class="input-box"> 
-                <input type="password" id="password" name="Institute_password" placeholder="Enter password" required>
+            <div class="field password">
+                <div class="input-area">
+                    <input type="password" id="password" name="Institute_password" placeholder="Password">
+                    <i class="icon fas fa-lock"></i>
+                    <i class="error error-icon fas fa-exclamation-circle"></i>
+                </div>
+                <div class="error error-txt">Password can't be blank</div>
             </div>
-            <div class="input-box button">
-                <input type="Submit" value="Login">
-            </div>
-            <div class="text">
-                {{-- <h3>Forget Password? <a href="#"></a></h3> --}}
-            </div>
+            <!-- <div class="pass-txt"><a href="#">Forgot password?</a></div> -->
+            <input type="submit" value="Login">
         </form>
     </div>
-    <script src="https://code.jquery.com/jquery-3.7.0.js" integrity="sha256-JlqSTELeR4TLqP0OG9dxM7yDPqX1ox/HfgiSLBj8+kM="
-        crossorigin="anonymous"></script>
-</body>
 
+    <script src="script.js"></script>
+</body>
 </html>
